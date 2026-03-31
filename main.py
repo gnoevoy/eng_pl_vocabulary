9c2a5399fd15e8ef63e00b7a3445a2b3e46df106
bars1k <gnoevoyandrey044@gmail.com>
2026-03-31 11:55:22 +0200

    created script for creating eng - pl words

--- main.py
+++ main.py
@@ -1,40 +1,37 @@
+from wordfreq import zipf_frequency
 import pandas as pd
-import numpy as np
-import random
 
 
-def get_values(df, col, n1, n2):
-    # pick up new random values
-    new_values = df.query("is_passed == False")
-    new_values_lst = random.choices(new_values[col].to_list(), k=n1)
+def get_words_with_score(eng_words_file, pl_words_file):
+    # Open txt files and save as lists
+    with open(f"data/{eng_words_file}.txt") as f:
+        en = f.read().splitlines()
 
-    # update table
-    df["is_passed"] = np.where(df[col].isin(new_values_lst), True, df["is_passed"])
+    with open(f"data/{pl_words_file}.txt") as f:
+        pl = f.read().splitlines()
+        pl[0] = "ten"
 
-    # select passed values
-    passed_values = df.query("is_passed == True")
-    passed_values_lst = random.choices(passed_values[col].to_list(), k=n2)
+    # Get a frequency usage score for each eng word
+    score = [zipf_frequency(word, "en", wordlist="best") for word in en]
 
-    return new_values_lst, passed_values_lst
+    return en, pl, score
+
+
+def get_csv_file(eng_words, pl_words, score):
+    # Create pandas dataframe
+    df = pd.DataFrame({"eng_word": eng_words, "pl_word": pl_words, "score": score})
+    mask = (df["eng_word"].str.len() > 2) & (df["score"] > 4)
+    df = df[mask].sample(frac=1).reset_index(drop=True)
+    df["id"] = range(1, len(df) + 1)
+
+    # Reorder columns and save as csv file
+    df.loc[:, ["id", "eng_word", "pl_word", "score"]].to_csv("data/words.csv", index=False, encoding="utf-8-sig")
+    print("CSV file created successfully.")
 
 
 def main():
-    # Import CSV files
-    words = pd.read_csv("data/words.csv")
-    phrases = pd.read_csv("data/phrases.csv")
-
-    new_words, passed_words = get_values(words, "word", 10, 10)
-    new_phrases, passed_phrases = get_values(phrases, "phrase", 3, 3)
-
-    # save changes to the CSV files
-    words.to_csv("data/words.csv", index=False)
-    phrases.to_csv("data/phrases.csv", index=False)
-
-    print("New words:", new_words)
-    print("Passed words:", passed_words)
-    print("")
-    print("New phrases:", new_phrases)
-    print("Passed phrases:", passed_phrases)
+    eng_words, pl_words, score = get_words_with_score("eng_words", "pl_words")
+    get_csv_file(eng_words, pl_words, score)
 
 
 if __name__ == "__main__":
